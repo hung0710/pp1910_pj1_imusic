b3d6cc7d6ecb9d2338d2554d1873c2545b5c2a09
Vũ Mạnh Hùng <ssvnn1996@gmail.com>
2020-05-08 09:21:50 +0700

    migrate

---
+++ database/migrations/2020_05_07_031651_create_songs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->integer('singer_id');
+            $table->text('lyrics');
+            $table->integer('category_id');
+            $table->integer('musician_id');
+            $table->string('decription', 255);
+            $table->enum('status',['approved','not approved']);
+            $table->integer('view');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
